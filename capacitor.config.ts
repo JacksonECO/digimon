36a7d5312c4516ab28936e3015d33d223be5734f
Jackson Galdino <jacksongdas.jackson@gmail.com>
2022-12-18 09:42:37 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.jacksoneco',
+  appName: 'digimon',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
